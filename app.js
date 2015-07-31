38a9063a657b12ce2fc4db7f7f1da78faf7bce27
乔治 <1975657371@qq.com>
2015-07-31 15:05:12 +0800

    bcrypt

--- app.js
+++ app.js
@@ -94,3 +94,59 @@ app.get('/admin/list',function(req,res){
 });
 
 console.log('on port' + port);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UserSchema.pre('save', function(next) {
+
+    var user = this
+
+    if (this.isNew) {
+        this.meta.createAt = this.meta.updateAt = Date.now()
+    }
+    else {
+        this.meta.updateAt = Date.now()
+    }
+
+    //如果用this就成 bcrypt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err)
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err)
+
+            user.password = hash
+            next()
+        })
+    })
+})
+
+UserSchema.methods = {
+    comparePassword: function(_password, cb) {
+        bcrypt.compare(_password, this.password, function(err, isMatch) {
+            if (err) return cb(err)
+
+            cb(null, isMatch)
+        })
+    }
+}
